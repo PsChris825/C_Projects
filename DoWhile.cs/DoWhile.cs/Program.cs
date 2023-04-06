4b1f5195c42fa4737511cb2c5e72753d336b3b22
Chris <pillotchristopher@gmail.com>
2023-04-06 14:14:47 -0700

    uploaded both while comparison and doWhile comparison

---
+++ DoWhile.cs/DoWhile.cs/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace DoWhile.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 76;
+            
+            do
+            {
+                switch (number)
+                {
+                    case 43:
+                        Console.WriteLine("You guessed 43. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 56:
+                        Console.WriteLine("You guessed 56. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 32:
+                        Console.WriteLine("You guessed 32. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 76:
+                        Console.WriteLine("You guessed 76. That is correct!");
+                        isGuessed |= true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+        }
+
+            
+    }
+}
