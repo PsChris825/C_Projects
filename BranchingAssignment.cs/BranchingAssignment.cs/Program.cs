fb9b55930bfc3cbf83c0a47050d2956b68c49504
Chris <pillotchristopher@gmail.com>
2023-04-07 15:30:40 -0700

    updated booleanlogic

--- BranchingAssignment.cs/BranchingAssignment.cs/Program.cs
+++ BranchingAssignment.cs/BranchingAssignment.cs/Program.cs
@@ -20,6 +20,10 @@ namespace BranchingAssignment.cs
                 return;
             }
 
+            Console.WriteLine("Please enter package width");
+            string width = Console.ReadLine();
+            int Pwidth = Convert.ToInt32(width);
+
             Console.WriteLine("Please enter package height");
             string height = Console.ReadLine();
             int Pheight = Convert.ToInt32(height);
@@ -28,9 +32,8 @@ namespace BranchingAssignment.cs
             string length = Console.ReadLine();
             int Plength = Convert.ToInt32(length);
 
-            
 
-            if ( Pheight > 50 || Plength > 50 )
+            if ( (Pheight + Plength + Pwidth) > 50 )
             {
                Console.WriteLine("Package is too big to be shipped via Package Express");
                 Console.ReadLine();
@@ -38,7 +41,7 @@ namespace BranchingAssignment.cs
             }
             
 
-            double totalD = (Pweight * Pheight * Plength) / 100;
+            double totalD = (Pweight * Pwidth * Pheight * Plength) / 100;
 
 
             Console.WriteLine("Your total for shipping this package is: " + "$" + totalD);
