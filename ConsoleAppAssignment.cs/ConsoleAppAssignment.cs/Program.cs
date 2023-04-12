b4ff7ec98da19be05497ffc15781a89ddc053fc8
Chris <pillotchristopher@gmail.com>
2023-04-11 20:48:29 -0700

    Created ConsoleApp Assignment
    part 1-6 ConsoleApp Assignment

---
+++ ConsoleAppAssignment.cs/ConsoleAppAssignment.cs/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+  static void Main(string[] args)
+  {
+        ////Part 1
+        //string[] manufacture = { "Honda ", "Nissan ", "BMW ", "Porsche ", "Audi ", "Ford ", };
+
+        //Console.WriteLine("Please type a color");
+        //string color = Console.ReadLine();
+
+        //for (int i = 0; i < manufacture.Length; i++)
+        //{
+        //    manufacture[i] +=  color;
+        //    Console.WriteLine(manufacture[i]);
+        //}
+
+        ////Part 2 Infinite loop
+        //int j = 0;
+        //while (true)
+        //{
+        //    Console.WriteLine("Cars are amazing!", j);
+            
+        //    j++;
+
+        //    //added an if statement to break the loop
+        //    if (j >= 5) 
+        //    {
+        //        break;
+        //    }
+        //}
+        
+        ////Part 3
+        //for (int C = 0; C < 7; C++) 
+        //{
+        //    Console.WriteLine("I go racing regularly");
+        //}
+
+        //for (int D = 0; D <= 1;  D++)
+        //{
+        //    Console.WriteLine("I love the Porsche 959");
+        //}
+
+        ////Part 4
+        //List<string> manufacturer = new List<string>() { "Honda", "Audi", "Mercedes", "Subaru", "Acura"};
+
+        //Console.WriteLine("Type a car manufacturer");
+        //string includes = Console.ReadLine();
+
+        //var matchManufacturer = -1;
+
+        //for (int E = 0;  E < manufacturer.Count; E++) 
+        //{ 
+        //    if (manufacturer[E] == includes)
+        //    {
+        //        matchManufacturer = E;
+        //        break;
+        //    }
+        //}
+
+        //if (matchManufacturer >= 0)
+        //{
+        //    Console.WriteLine("Index: " +  matchManufacturer);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("This is not included in the list");
+        //}
+
+        //Part 5
+        //List<string> same = new List<string>() { "cat", "dog", "dog", "iguana", "hamster", "snake"};
+        
+        //Console.WriteLine("Please type a type of pet");
+        //string pet = Console.ReadLine();
+
+        //var petmatch = -1;
+
+        //for (int F = 0; F < same.Count; F++)
+        //{
+        //    if (same[F] == pet) 
+        //    {
+        //        petmatch = F;
+        //        break;
+        
+        //    }
+        //}
+        
+        //if (petmatch >= 0)
+        //{
+        //    Console.WriteLine("Index: " + petmatch);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("This is not included in the list");
+        //}
+
+        //Part 6
+        List<string> names = new List<string>() { "Chris", "John", "Justin", "D", "Chris"};
+
+        List<string> names2 = new List<string>();
+
+        foreach (string name in names) 
+        {
+            if (names2.Contains(name))
+            {
+                Console.WriteLine(name + " Already exists in list");
+            }
+            else
+            {
+                names2.Add(name);
+                Console.WriteLine(name);
+            }
+        }
+
+
+       
+
+
+        Console.ReadLine();
+  }
+}
+
---
+++ IterationPractice.cs/IterationPractice.cs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+  static void Main(string[] args)
+    {
+        //int[] testScore = { 88, 99, 65, 70, 82, 34, 91, 90, 84 };
+
+        //for (int i = 0; i < testScore.Length; i++) 
+        //{
+        //    if (testScore[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testScore[i]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //string[] names = { "J", "B", "C", "D" };
+
+        //for (int i = 0; i < names.Length; i++) 
+        //{
+        //    if (names[i] == "J")
+        //    {
+        //        Console.WriteLine(names[i]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //List<int> testScores = new List<int>();
+        //testScores.Add(98);
+        //testScores.Add(99);
+        //testScores.Add(88);
+        //testScores.Add(81);
+        //testScores.Add(72);
+        //testScores.Add(85);
+
+        //foreach (int score in testScores) 
+        //{
+        //    if (score > 85)
+        //    {
+        //        Console.WriteLine("Passing Test score: " +  score);
+        //    }
+        //}
+        //Console.ReadKey();  
+
+        List<string> list = new List<string>() { "Jesse", "Eric", "Chris", "Adam" };
+
+        foreach (var item in list) 
+        {
+            if (item == "Jesse")
+            {
+                Console.WriteLine(item);
+            }
+        }
+        Console.ReadLine();
+    }
+}
