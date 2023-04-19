31e14bcf826ba36f86878c3d6898b42042324357
Chris <pillotchristopher@gmail.com>
2023-04-18 17:12:16 -0700

    calling method assignment

---
+++ Calling Methods.cs/Calling Methods.cs/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods.cs
+{
+    public class MathClass
+    {
+        public static int AddFive(int num)
+        {
+            return num + 5;
+        }
+
+        public static int DivideTen(int num)
+        {
+            return num / 10;
+        }
+
+        public static int MultiplyTwo(int num)
+        {
+            return num * 2;
+        }
+    }
+}  
---
+++ Calling Methods.cs/Calling Methods.cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("AddFive: " + MathClass.AddFive(num));
+            Console.WriteLine("DivideTen: " + MathClass.DivideTen(num));
+            Console.WriteLine("MultiplyTwo: " + MathClass.MultiplyTwo(num));
+
+            Console.ReadLine();
+        }
+    }
+}
