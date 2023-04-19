8e554d7393d3aa0b07ed77ce763efa973dd4e9fd
Chris <pillotchristopher@gmail.com>
2023-04-19 16:51:29 -0700

    Calling method assignment

---
+++ MainMethod.cs/MainMethod.cs/Method.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MathOperations
+{
+    public int PerformOperation(int num)
+    {
+        // Perform a math operation on the integer input
+        int result = num + 10;
+
+        // Return the result as an integer
+        return result;
+    }
+
+    public int PerformOperation(double num)
+    {
+        // Perform a different math operation on the decimal input
+        int result = (int)(num * 10);
+
+        // Return the result as an integer
+        return result;
+    }
+
+    public int PerformOperation(string numString)
+    {
+        // Convert the string input to an integer, if possible
+        int num;
+        if (int.TryParse(numString, out num))
+        {
+            // Perform a different math operation on the integer input
+            int result = num / 5;
+
+            // Return the result as an integer
+            return result;
+        }
+        else
+        {
+            // If the string cannot be converted to an integer, return an error code
+            return -1;
+        }
+    }
+}
---
+++ MainMethod.cs/MainMethod.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOps = new MathOperations();
+
+            // Call the first method with an integer input
+            int intResult = mathOps.PerformOperation(5);
+            Console.WriteLine("Result of integer operation: " + intResult);
+
+            // Call the second method with a decimal input
+            int decimalResult = mathOps.PerformOperation(3.14159);
+            Console.WriteLine("Result of decimal operation: " + decimalResult);
+
+            // Call the third method with a string input
+            int stringResult = mathOps.PerformOperation("25");
+            Console.WriteLine("Result of string operation: " + stringResult);
+
+            Console.ReadLine();
+        }
+        
+    }
+    
+}
