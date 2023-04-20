ad11c251af4c0a6ff7d778351a75952069e6b996
Chris <pillotchristopher@gmail.com>
2023-04-19 18:46:08 -0700

    ClassSubmission1

---
+++ ClassSubmission1/ClassSubmission1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            mathOp math = new mathOp();
+            int product;
+
+
+            // ask to enter number
+            Console.WriteLine("Enter number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            math.MathOperation(num1);
+
+            Console.WriteLine("Enter first number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int num3 = int.Parse(Console.ReadLine());
+
+            math.OutPutParam(num2, num3, out product);
+
+            int sumInt = math.Add(5, 10);
+            Console.WriteLine(sumInt);
+            double sumDouble = math.Add(4.21, 5.39);
+            Console.WriteLine(sumDouble);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassSubmission1/ClassSubmission1/mathop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class mathOp
+{
+    public void MathOperation(int num)
+    {
+        int result = num / 2;
+
+        Console.WriteLine(result);
+    }
+
+    public void OutPutParam(int num2, int num3, out int product)
+    {
+        product = num2 * num3;
+
+        Console.WriteLine(product);
+    }
+
+    public int Add(int num4,  int num5) 
+    {
+        return num4 + num5;
+    }
+
+    public double Add(double num4, double num5)
+    {
+        return num4 + num5;
+    }
+}
