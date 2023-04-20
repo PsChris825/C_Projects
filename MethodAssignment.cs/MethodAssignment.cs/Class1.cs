49fabb76d206666639825710c75970f7ccb35935
Chris <pillotchristopher@gmail.com>
2023-04-19 17:24:47 -0700

    Methods Assignment uploaded

---
+++ MethodAssignment.cs/MethodAssignment.cs/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MathOp
+{
+    public int PerformOp(int num1, int num2 = 20)
+    {
+        //Perform a math operation on input parameter
+        int result = num1 + num2;
+
+        //return the result as an integer
+        return result;
+
+    }
+
+
+}
---
+++ MethodAssignment.cs/MethodAssignment.cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment.cs
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            MathOp Math = new MathOp();
+
+            Console.WriteLine("Please enter first number");
+            string firstNum = Console.ReadLine();
+            int NumOne = Int32.Parse(firstNum);
+
+            Console.WriteLine("Please enter second number (press enter to skip)");
+            string secondNum = Console.ReadLine();
+            int Numtwo = secondNum == "" ? 20: int.Parse(secondNum);
+
+            //Call the method wit htwo integer inputs
+            int result1 = Math.PerformOp(NumOne, Numtwo);
+            Console.WriteLine(result1);
+
+            //Call the method with one integer input
+            int result2 = Math.PerformOp(NumOne);
+            Console.WriteLine(result2);
+
+            Console.ReadLine();
+        }
+    }
+}
