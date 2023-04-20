cb9fbf645d03e975cc095cd39c74fcd1163e8056
Chris <pillotchristopher@gmail.com>
2023-04-19 17:40:24 -0700

    methodAssignment2 uploaded

---
+++ MethodAssignment2.cs/MethodAssignment2.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOps math = new MathOps();
+
+            // Call the method with two parameters
+            math.PerformOperation(5, 10);
+
+            // Call the method with named parameters
+            math.PerformOperation(num1: 2, num2: 40);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodAssignment2.cs/MethodAssignment2.cs/voidClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MathOps
+{
+    public void PerformOperation(int num1, int num2)
+    {
+        //Perform a math operation on the first int
+        int result = num1 + 10;
+
+        //Display the secodn int to the console
+        Console.WriteLine("Second interger:" + num2);
+
+        //Output the result of the math operation to the console
+        Console.WriteLine("Result: " +  result);
+    }
+}
