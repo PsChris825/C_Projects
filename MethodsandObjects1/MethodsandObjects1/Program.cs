5f338ccb129d067797785b748b8594568c07d197
Chris <pillotchristopher@gmail.com>
2023-04-20 19:31:29 -0700

    Methods and Objectives

---
+++ MethodsandObjects1/MethodsandObjects1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsandObjects1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.Id = 12345;
+
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
