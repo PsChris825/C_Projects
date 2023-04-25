3ec5fafd4b5494f20f68c00d339a9e17c261eb63
Chris <pillotchristopher@gmail.com>
2023-04-25 16:44:24 -0700

    Abstract assignment

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -9,22 +9,21 @@ namespace TwentyOne
     class Program
     {
         static void Main(string[] args) 
-        { 
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Bill", "Joe"};
+        {
+           TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
             game.ListPlayers();
-            game.play();
             Console.ReadLine();
 
-        //   Deck deck = new Deck();
-        //   deck.Shuffle(3);
-           
-        //   foreach (Card card in deck.Cards) 
-        //   {
-        //       Console.WriteLine(card.Face + " of " + card.Suit);
-        //   }
-        //   Console.WriteLine(deck.Cards.Count);
-        //   Console.ReadLine();
-        //}
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
+        }
     }
 }
